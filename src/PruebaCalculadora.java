165a66a9369c68d2ec887dc9fd827a92f550bc4e
Bryan Valdez <bryan501th@gmail.com>
2021-02-12 14:08:20 -0600

    Se creó el método inst que ordena los componentes en un sistema de cordenadas, también se ordenaron todos los componentes

--- src/PruebaCalculadora.java
+++ src/PruebaCalculadora.java
@@ -18,9 +18,8 @@ class Calculadora extends JFrame implements ActionListener{
 	GridBagLayout gbl = new GridBagLayout();
 	GridBagConstraints gbc = new GridBagConstraints();
 	
-	JTextField areaTexto = new JTextField();
-	
-	JButton btnSumar = new JButton("+");
+	JTextField areaTexto = new JTextField();//caja
+	JButton btnSumar = new JButton("+");//operaciones
 	JButton btnRestar = new JButton("-");
 	JButton btnMultiplicar = new JButton("*");
 	JButton btnDividir = new JButton("/");
@@ -30,8 +29,7 @@ class Calculadora extends JFrame implements ActionListener{
 	JButton btnRaiz = new JButton("sqrt");
 	JButton btnDel = new JButton("<[]");
 	JButton btnCE = new JButton("CE");
-	JButton btnResultado = new JButton("=");
-	
+	JButton btnResultado = new JButton("=");//numeros
 	JButton btn0 = new JButton("0");
 	JButton btn1 = new JButton("1");
 	JButton btn2 = new JButton("2");
@@ -76,7 +74,47 @@ class Calculadora extends JFrame implements ActionListener{
 		btn9.addActionListener(this);
 		btnDot.addActionListener(this);
 		
+		inst(areaTexto,0,0,4,1,GridBagConstraints.BOTH);
 		
+		inst(btnResiduo,0,1,1,1,GridBagConstraints.BOTH);
+		inst(btnCE,1,1,1,1,GridBagConstraints.BOTH);
+		inst(btnDel,2,1,2,1,GridBagConstraints.BOTH);
+		
+		inst(btn1X,0,2,1,1,GridBagConstraints.BOTH);
+		inst(btnPotencia,1,2,1,1,GridBagConstraints.BOTH);
+		inst(btnRaiz,2,2,1,1,GridBagConstraints.BOTH);
+		inst(btnDividir,3,2,1,1,GridBagConstraints.BOTH);
+		
+		inst(btn7,0,3,1,1,GridBagConstraints.BOTH);
+		inst(btn8,1,3,1,1,GridBagConstraints.BOTH);
+		inst(btn9,2,3,1,1,GridBagConstraints.BOTH);
+		inst(btnMultiplicar,3,3,1,1,GridBagConstraints.BOTH);
+		
+		inst(btn4,0,4,1,1,GridBagConstraints.BOTH);
+		inst(btn5,1,4,1,1,GridBagConstraints.BOTH);
+		inst(btn6,2,4,1,1,GridBagConstraints.BOTH);
+		inst(btnRestar,3,4,1,1,GridBagConstraints.BOTH);
+		
+		inst(btn1,0,5,1,1,GridBagConstraints.BOTH);
+		inst(btn2,1,5,1,1,GridBagConstraints.BOTH);
+		inst(btn3,2,5,1,1,GridBagConstraints.BOTH);
+		inst(btnSumar,3,5,1,1,GridBagConstraints.BOTH);
+
+		inst(btnDot,0,6,1,1,GridBagConstraints.BOTH);
+		inst(btn0,1,6,1,1,GridBagConstraints.BOTH);
+		inst(btnResultado,2,6,2,1,GridBagConstraints.BOTH);
+		
+		pack();
+	}
+	
+	public void inst(Component cmp,int gridx, int gridy, int gridwidth, int gridheight, int fill) {
+		gbc.gridx=gridx;
+		gbc.gridy=gridy;
+		gbc.gridwidth=gridwidth;
+		gbc.gridheight=gridheight;
+		gbc.fill=fill;
+		gbl.setConstraints(cmp, gbc);
+		add(cmp);
 	}
 	
 	@Override
